ce20f28da9f70744844745140de5fd89eadbc0d6
Rose Crisp <rocrisp@redhat.com>
2023-01-26 09:08:41 -0500

    stable version

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
-export VERSION=0.0.14
+export VERSION=0.1.0
 export IMG=quay.io/rocrisp/kubetruth-operator:v$VERSION
 export BUNDLE_IMG=quay.io/rocrisp/kubetruth-operator-bundle:v$VERSION
 export CATALOG_IMG=quay.io/rocrisp/kubetruth-operator-catalog:v$VERSION
